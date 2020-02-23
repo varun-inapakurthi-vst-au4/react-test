b770f9cbb093d9ea565902d54bb4bf682a2c99a9
Varun Inapakurthi <princevarun78@gmail.com>
2020-02-23 15:26:37 +0530

    Added.

---
+++ Q2/src/components/class.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Clas extends Component {
+    state = {}
+    render() {
+        return (<div>
+            <p>This is a Class component.</p>
+        </div>);
+    }
+}
+
+export default Clas;
